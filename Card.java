bec422bda51ad6a19739ba8e19ea96a638985311
Robert Baxter Jr <rsb2194@columbia.edu>
2022-05-28 10:54:56 -0400

    Card Class
    Creating skeleton of a card and two accessor methods to get the rank and suit.

---
+++ Card.java
@@ -0,0 +1,35 @@
+class Card {
+    
+    private char suit;
+    private int rank;
+
+    public Card(char suit, int rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public char getSuit(){
+        return suit;
+    }
+
+    public int getRank(){
+        return rank;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
