a2d0680ab82812b34dad13fc208bfef6e1cb8a86
Robert Baxter Jr <rsb2194@columbia.edu>
2022-06-01 12:36:45 -0400

    Update Player.java
    Nothing really changed.

---
+++ Player.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Player {
+
+    private ArrayList<Card> playerCard;
+    private Scanner scan;
+    private String response;
+
+    public Player(){
+        scan = new Scanner(System.in);
+        playerCard = new ArrayList<Card>();
+    }
+
+    public int playsTurn(){
+        
+    }
+    
+}
