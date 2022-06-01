974353ced96642f8cf6dff97e7feda6ec5eb5551
Robert Baxter Jr <rsb2194@columbia.edu>
2022-06-01 12:35:53 -0400

    Update Deck.java
    Instead of shuffling deck, a random card is picked each time when drawing. Also deck is now an ArrayList rather than a string.

--- Deck.java
+++ Deck.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Random;
 
+import javax.lang.model.element.ElementVisitor;
+
 class Deck {
-    private Card[] deck = new Card[52];
+    private ArrayList <Card> deck = new ArrayList<>();
     public int top;
     public static final char[] suits = {'c', 'h', 's', 'd'};\
     public static final int[] rank = {0,1,2,3,4,5,6,7,8,9,10,11,12};
@@ -11,29 +14,25 @@ class Deck {
     public String faceUpString;
 
     public Deck(){
-        int counter = 0;
-        int top = 0;
         for(int i = 0;i<suits.length;i++){
             for(int j = 0;j<rank.length;j++){
-                deck[counter] = new Card(suits[i], rank[j]);
-                counter++;
+                deck.add(new Card(suits[i], rank[j]));
             }
         }
     }
 
-    public Card deal(){
-        top = top+1;
-        return deck[top-1];
-    }
-
-    public void shuffle(){
+    public Card draw(){
         Random rand = new Random();
-        for(int i = 0;i<15000;i++){
-            int x = (int) (Math.random()*deck.length);
-            int y = (int) (Math.random()*deck.length);
-            Card temp = deck[x];
-            deck[x]=deck[y];
-            deck[y]=temp;
+        int index = rand.nextInt(deck.size());
+        return deck.remove(index);
+        }
+
+    public boolean canDeal(){
+        if(deck.isEmpty()){
+            return false;
+        }
+        else{
+            return true;
         }
     }
 
